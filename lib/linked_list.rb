36db5136aa0ec7b575a9dfdc2c4eb5802a602671
Hillary Muller <hillaryaward@gmail.com>
2023-03-20 14:54:31 -0500

    commit tree aed5fb93e58207355c6278bc2330039a039eceb7

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,14 +1,30 @@
 require_relative './node'
 
 class LinkedList
-  attr_accessor :head, :tail
+  attr_accessor :head, :tail, :length
 
   def initialize
     @head = nil
     @tail = nil
+    @length = 0
   end
 
   def reverse!
-    # your code here
+    return self if self.length < 2
+    curr = @head
+    @head = @tail
+    @tail = curr
+
+    i = 0
+    while i < @length do
+      prev = curr.next_node
+      curr.next_node = curr.prev_node
+      curr.prev_node = prev
+      curr = prev
+      i += 1
+    end
+
+    return self
+
   end
 end
